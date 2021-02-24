9551cc38e0f174177044ab7637265bcff6a65bcd
Max Denner <maxdenner027@hotmail.com>
2021-02-23 22:53:01 -0300

    add: classes de modelo adicionadas.

--- backend/src/main/java/br/com/esiggroup/recrutamento/taskmanager/models/Responsavel.java
+++ backend/src/main/java/br/com/esiggroup/recrutamento/taskmanager/models/Responsavel.java
@@ -1,5 +1,41 @@
 package br.com.esiggroup.recrutamento.taskmanager.models;
 
-public class Responsavel {
-	
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "responsaveis")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Responsavel implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_responsavel", nullable = false, unique = true)
+	private Long idResponsavel;
+
+	@Column(name = "nome_responsavel", unique = true)
+	private String nomeResponsavel;
+
+	@Setter(value = AccessLevel.NONE)
+	@OneToMany(mappedBy = "responsavelTarefa", targetEntity = Tarefa.class)
+	private Set<Tarefa> tarefasResponsavel;
 }
--- backend/src/main/java/br/com/esiggroup/recrutamento/taskmanager/models/Tarefa.java
+++ backend/src/main/java/br/com/esiggroup/recrutamento/taskmanager/models/Tarefa.java
@@ -1,5 +1,58 @@
 package br.com.esiggroup.recrutamento.taskmanager.models;
 
-public class Tarefa {
+import java.io.Serializable;
+import java.util.Calendar;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import br.com.esiggroup.recrutamento.taskmanager.enums.PrioridadeTarefa;
+import br.com.esiggroup.recrutamento.taskmanager.enums.StatusTarefa;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tarefas")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tarefa implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_tarefa", nullable = false, unique = true)
+	private Long idTarefa;
+
+	@Column(name = "titulo_tarefa", nullable = false)
+	private String tituloTarefa;
+
+	@Column(name = "descricao_tarefa", nullable = false)
+	private String descricaoTarefa;
+
+	@Column(name = "prioridade_tarefa", nullable = false)
+	private PrioridadeTarefa prioridadeTarefa;
+
+	@Column(name = "status_tarefa", nullable = false)
+	private StatusTarefa statusTarefa;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "deadline_tarefa", nullable = false)
+	private Calendar deadlineTarefa;
+
+	@ManyToOne(targetEntity = Responsavel.class)
+	@JoinColumn(name = "id_responsavel_tarefa")
+	private Responsavel responsavelTarefa;
 }
